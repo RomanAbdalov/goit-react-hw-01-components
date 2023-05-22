abbf631f8925d6b48cd3a7087c95938683762996
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-05-22 19:07:02 +0300

    add marcup profile, App,index.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Profile from './Profile';
+import user from '../user.json';
+export default function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+}
